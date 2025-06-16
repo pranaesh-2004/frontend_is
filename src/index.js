ca3b6cf202a059014de095513364e9f6f9f74b65
pranaesh-2004 <pranaesh19@gmail.com>
2025-06-16 18:34:52 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -12,18 +12,18 @@ import 'react-toastify/dist/ReactToastify.css';
 import { LoadingProvider } from './hooks/useLoading';
 import './interceptors/authInterceptor';
 
-<<<<<<< HEAD
+
 // Google Fonts (move this to index.html instead)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode> // REMOVE THIS LINE TO PREVENT DOUBLE RENDER IN DEV
-=======
+
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
->>>>>>> 4228c54e0c651cc8601ee0dcbc07ab673a979434
+
     <BrowserRouter>
       <LoadingProvider>
         <AuthProvider>
@@ -49,12 +49,12 @@ root.render(
   // </React.StrictMode> // REMOVE THIS LINE TOO
 );
 
-=======
+
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
->>>>>>> 4228c54e0c651cc8601ee0dcbc07ab673a979434
+
 reportWebVitals();
